2f2d1812b9d7047ff44bca719e6efa91336629bf
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-04 14:53:17 +0100

    feat: aggiunte rotte

--- afp-his/src/app/app.routes.ts
+++ afp-his/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { ListaPz } from './pattern/lista-pz/lista-pz';
+import { ModificaPz } from './features/modifica-pz/modifica-pz';
+import { AccettazionePz } from './features/accettazione-pz/accettazione-pz';
+import { StatoPz } from './features/stato-servizi/stato-pz';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'lista-pz',
+        component: ListaPz,
+    },
+    {
+        path: 'modifica-pz:id',
+        component: ModificaPz
+    },
+    {
+        path: 'accettazione-pz',
+        component: AccettazionePz
+    },
+    {
+        path: 'stato-pz',
+        component: StatoPz
+    },
+    {
+        path: '',
+        redirectTo: 'lista-pz',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'lista-pz',
+        pathMatch: 'full'
+    }
+];
