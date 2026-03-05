3f638f26e0fcbd58997b154cfc1c8ea148571697
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-05 12:12:22 +0100

    fix: aggiornata la funzione dark mode toggle con il cambio di icona quando si va in modalita scura

--- afp-his/src/app/ui/darkmode-selector/darkmode-selector.ts
+++ afp-his/src/app/ui/darkmode-selector/darkmode-selector.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'his-darkmode-selector',
+  standalone: true,
   imports: [ButtonModule],
   templateUrl: './darkmode-selector.html',
   styleUrl: './darkmode-selector.scss',
 })
-export class DarkmodeSelector {
+export class DarkmodeSelector implements OnInit {
+  isDark: boolean = false;
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.isDark = prefersDark;
+    
+    if (this.isDark) {
+      document.querySelector('html')?.classList.add('my-app-dark');
+    }
+  }
+
   toggleDarkMode() {
     const element = document.querySelector('html');
-    element?.classList.toggle('my-app-dark');
+    this.isDark = element?.classList.toggle('my-app-dark') ?? false;
   }
 }
