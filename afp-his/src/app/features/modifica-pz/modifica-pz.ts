87709468bb4613feaaf8eaf4eca056f182b1ff0c
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-12 11:32:06 +0100

    feat: aggiunta gestione dei dati e logica di visualizzazione nel componente modifica-pz

--- afp-his/src/app/features/modifica-pz/modifica-pz.ts
+++ afp-his/src/app/features/modifica-pz/modifica-pz.ts
@@ -1,14 +1,20 @@
+import { httpResource } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, effect, input } from '@angular/core';
+import { PazienteDTO } from '../../core/Pazienti/Pazienti.model';
+import { APIResponse } from '../../core/models/APIResponse.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'his-modifica-pz',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './modifica-pz.html',
   styleUrl: './modifica-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModificaPz {
   patientID = input<string>();
+
+  patient = httpResource<APIResponse<PazienteDTO>>(() => `http://localhost:3000/admissions/${this.patientID()}`, {});
 
   constructor() {
     effect(() => {
