5761029a582258756de8921fdd021b9f1b50d45b
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-19 12:03:23 +0100

    feat: creato campo input con gestione errori

--- afp-his/src/app/features/accettazione-pz/accettazione-pz.ts
+++ afp-his/src/app/features/accettazione-pz/accettazione-pz.ts
@@ -1,16 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GestioneRisorse } from '../../core/Risorse/gestione-risorse';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'his-accettazione-pz',
-  imports: [ JsonPipe ],
+  imports: [InputTextModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './accettazione-pz.html',
   styleUrl: './accettazione-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccettazionePz {
   GestioneRisorse = inject(GestioneRisorse);
+  nome = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]);
 
 
 }
