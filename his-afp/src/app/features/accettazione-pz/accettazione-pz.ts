068b4448b68112a1a1cd58e8cf9d24f929faf222
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-03-19 11:00:05 +0100

    feat: ✨ add form control for patient name with validation
    Implement a reactive form control for the patient name input, including validation for required, minimum, and maximum length. Update the template to display the status of the form control, indicating whether the name has been modified, touched, valid, or pending. This enhances user feedback during patient data entry.

--- his-afp/src/app/features/accettazione-pz/accettazione-pz.ts
+++ his-afp/src/app/features/accettazione-pz/accettazione-pz.ts
@@ -1,14 +1,20 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GestioneRisorse } from '../../core/Risorse/gestione-risorse';
-import { JsonPipe } from '@angular/common';
+import { InputText } from 'primeng/inputtext';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'his-accettazione-pz',
-  imports: [JsonPipe],
+  imports: [InputText, ReactiveFormsModule],
   templateUrl: './accettazione-pz.html',
   styleUrl: './accettazione-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccettazionePz {
   gestioneRisorse = inject(GestioneRisorse);
+  nome = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(30),
+  ]);
 }
