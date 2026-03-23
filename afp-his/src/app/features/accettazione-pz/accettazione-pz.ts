daae8ecc3c3a36c072515603b4ffe4ee708b77db
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-23 13:03:09 +0100

    feat: aggiunte nuove funzioni e controlli del form

--- afp-his/src/app/features/accettazione-pz/accettazione-pz.ts
+++ afp-his/src/app/features/accettazione-pz/accettazione-pz.ts
@@ -1,19 +1,63 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GestioneRisorse } from '../../core/Risorse/gestione-risorse';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'his-accettazione-pz',
-  imports: [InputTextModule, ReactiveFormsModule, JsonPipe],
+  imports: [InputTextModule, ReactiveFormsModule, JsonPipe, Button],
   templateUrl: './accettazione-pz.html',
   styleUrl: './accettazione-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccettazionePz {
-  GestioneRisorse = inject(GestioneRisorse);
-  nome = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]);
+  gestioneRisorse = inject(GestioneRisorse);
 
+  // paziente = new FormGroup({
+  //   nome: new FormControl('', [Validators.required]),
+  //   cognome: new FormControl('', [Validators.required]),
+  // });
 
+  readonly #fb = inject(FormBuilder);
+
+  paziente = this.#fb.group({
+    anagrafica: this.#fb.group({
+      nome: ['', Validators.required],
+      cognome: ['', Validators.required],
+      dataNascita: ['', Validators.required],
+      codiceFiscale: ['', Validators.required],
+      sesso: ['', Validators.required],
+    }),
+    sanitaria: this.#fb.group({
+      patologia: ['', Validators.required],
+      codiceColore: ['', Validators.required],
+      modArrivo: ['', Validators.required],
+      noteTriage: ['', Validators.required],
+    })
+  })
+
+  checkFormControl(control: string) {
+    const fc = this.paziente.get(control);
+    return fc?.invalid && (fc.touched || fc.dirty);
+  }
+
+  checkFormControlErrorl(control: string, err: string) {
+    const fc = this.paziente.get(control);
+
+    if (fc && fc.hasError(err)) {
+      return fc.getError(err);
+    } else {
+      return null
+    }
+  }
+
+  onSubmit() {
+    if(this.paziente.valid){
+    console.log(this.paziente.value);
+    } else {
+      this.paziente.markAllAsTouched();
+    }
+  }
 }
